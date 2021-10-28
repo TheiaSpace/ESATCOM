512000095528251220c2f7e6050455210bab48c1
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2021-10-28 13:46:26 +0200

    Bump version number to 1.1.0

--- examples/GS/GS.ino
+++ examples/GS/GS.ino
@@ -1,5 +1,5 @@
 /*
- * ESAT COM Ground Station Main Program version 1.0.1
+ * ESAT COM Ground Station Main Program version 1.1.0
  * Copyright (C) 2020, 2021 Theia Space, Universidad Politécnica
  * de Madrid.
  *
@@ -33,8 +33,8 @@
 
 const word GS_APPLICATION_PROCESS_IDENTIFIER = 6;
 const byte GS_MAJOR_VERSION_NUMBER = 1;
-const byte GS_MINOR_VERSION_NUMBER = 0;
-const byte GS_PATCH_VERSION_NUMBER = 1;
+const byte GS_MINOR_VERSION_NUMBER = 1;
+const byte GS_PATCH_VERSION_NUMBER = 0;
 
 ESAT_CCSDSPacket packet(ESAT_COMClass::PACKET_DATA_BUFFER_LENGTH);
 
